d7a47ce1b7cad67a595ac44b070de4dc4c6b2c5b
zhongwangwei <lifeecho@live.com>
2024-10-30 13:51:33 +0800

    bug fixed for stn plot

--- script/figlib/Fig_taylor_diagram.py
+++ script/figlib/Fig_taylor_diagram.py
@@ -58,8 +58,8 @@ def make_scenarios_comparison_Taylor_Diagram(basedir, evaluation_item, STDs, RMS
                    markerobs=option['markerobs'], markersizeobs=option['markersizeobs'],
                    )
 
-    #if not option['title']:
-    #    option['title'] = evaluation_item.replace('_', " ")
+    if not option['title']:
+        option['title'] = evaluation_item.replace('_', " ")
     #ax.set_title(option['title'], fontsize=option['title_size'], pad=30)
 
     output_file_path = os.path.join(f'{basedir}', 'output', 'comparisons', 'Taylor_Diagram',
@@ -2547,6 +2547,6 @@ def generate_markers(data_names, option):
             "edgeColor": color,
             "symbol": next(symbols),
             "size": option['MARKERSsize'],
-            "faceColor": 'none',
+            "faceColor": faceColor,
         }
     return markers
