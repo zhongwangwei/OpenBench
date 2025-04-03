841cc31cb249bcac3963ae0d68fcbda556841d69
xuqch3 <xuqch3@mail2.sysu.edu.cn>
2025-04-03 20:30:59 +0800

    Update logging info

--- script/custom/SMAP_L4_filter.py
+++ script/custom/SMAP_L4_filter.py
@@ -1,10 +1,11 @@
-def filter_SMAP_L4(info,ds):   
+import logging
+def filter_SMAP_L4(info,ds):
    if info.item == "Net_Radiation":
       try:
          ds['Net_Radiation'] = ds['net_downward_longwave_flux'] + ds['net_downward_shortwave_flux'] 
          info.sim_varname = 'Net_Radiation'
          info.sim_varunit = 'W m-2'
       except Exception as e:
-         print(f"Surface Net Radiation calculation processing ERROR: {e}")
+         logging.error(f"Surface Net Radiation calculation processing ERROR: {e}")
          return info, None
       return info, ds['Net_Radiation']
